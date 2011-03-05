9d81add789b19f5df5883ed8d09d0ad0f48a711f
Lishaak Bystroushaak <bystrousak@kitakitsune.org>
2011-03-05 22:33:28 +0100

    Přidána metoda jenž umí parsovat parametry tagu + hlavička vyhledávací fce.

--- dhtmlparser.d
+++ dhtmlparser.d
@@ -2,11 +2,12 @@
  * dhtmlparser.d v0.1.0 (03.03.2011) by Bystroushaak (bystrousak@kitakitsune.org)
  * 
  * TODO:
-	 * přidělat parsování argumentů tagu
 	 * pretiffy by měla replacovat "    " za " ", nebrat v ůvahu řádky plné mezer (nikoli prázdné!)
 	 * zapouzdřit HTMLElement
 	 * přepsat pretiffy tak, aby vytvářela pole stringů, které pak vrátí jako jeden string namísto writeln..
-	 * promyslet a přidat vyhledávací fce
+	 * promyslet a přidat vyhledávací a porovnávací metody, nějaký dotazovač
+	 * přidat možnost vyhledávání podle vlastní fce..
+	 * přidělat transformační fce - setIsComment
 */ 
 
 import std.string;
@@ -22,23 +23,40 @@ class HTMLParserException:Exception{
 class HTMLElement{
 	private string element, tagname;
 	private bool istag, isendtag, iscomment, isnonpairtag;
+	
 	public HTMLElement[] childs;
 	public HTMLElement endtag, openertag;
-	public HTMLElement nexttag;
+	public string[string] params;
 	
 	this(string str){
 		this.element = str;
 		
 		this.parseIsTag();
 		this.parseIsEndTag();
-		this.parseIsNonPairTag();
+		
 		this.parseIsComment();
+		
 		if (!this.istag || this.iscomment)
 			this.tagname = element;
 		else
 			this.parseTagName();
+		
+		this.parseIsNonPairTag();
+		
+		if (this.isOpeningTag())
+			this.parseParams();
 	}
 	
+	public HTMLElement[] find(string tag_name, string[string] tag_arguments = null){
+		if (!this.isTag() || this.isComment() || this.isNonPairTag() || this.childs.length <= 0)
+			return null;
+		
+		
+	}
+	
+	/***************************************************************************
+	 * PARSERS *****************************************************************
+	 **************************************************************************/ 
 	private void parseIsTag(){
 		if (this.element.startsWith("<") && this.element.endsWith(">"))
 			this.istag = true;
@@ -88,9 +106,11 @@ class HTMLElement{
 		];
 		
 		// Check listed nonpair tags
-		foreach(string tag; npt){
-			if (tag == this.getTagName())
+		foreach(string tagname; npt){
+			if (tagname == this.tagname){
 				this.isnonpairtag = true;
+				break;
+			}
 		}
 	}
 	
@@ -106,10 +126,58 @@ class HTMLElement{
 			el = el.replace("/", "").replace("<", "").replace(">", "");
 			if (el.length > 0){
 				this.tagname = el;
-				break;
+				return;
 			}
 		}
 	}
+	
+	private void parseParams(){
+		if (this.element.indexOf(" ") <= 0 || this.element.indexOf("=") <= 0)
+			return;
+		
+		// Remove '<' & '>'
+		string params = this.element[1 .. $-1].strip();
+		
+		// Remove tagname
+		params = params[params.indexOf(" ") .. $].strip();
+		
+		string[] tmp = params.split("=");
+		
+		// Len must be even-numbered
+		uint tmp_len;
+		if ((tmp.length % 2) == 0)
+			tmp_len = tmp.length;
+		else if ((tmp.length %2) == 1 && tmp.length > 1)
+			tmp_len = tmp.length - 1;
+		else
+			return;
+		
+		// Parse parameters (it isnt so simple as it could look..)
+		uint li; // last index
+		string value, key = tmp[0];
+		for(uint i = 1; i <= tmp_len - 1; i++){
+			li = tmp[i].lastIndexOf(" ");
+			li = (li < tmp[i].lastIndexOf("'") ? li : tmp[i].lastIndexOf("'"));
+			li = (li < tmp[i].lastIndexOf("\"") ? li : tmp[i].lastIndexOf("\""));
+			
+			this.params[key] = tmp[i][0 .. li + 1];
+			key = tmp[i][li + 1 .. $];
+		}
+		this.params[key] = tmp[$ - 1];
+		
+		// Remove craps from parameters
+		foreach(pkey, pvalue; this.params){
+			if (this.params[pkey].startsWith("'") || this.params[pkey].startsWith("\""))
+				this.params[pkey] = this.params[pkey][1 .. $];
+			if (this.params[pkey].endsWith("'") || this.params[pkey].endsWith("\""))
+				this.params[pkey] = this.params[pkey][0 .. $ - 1];
+		}
+	}
+	//* /Parsers ***************************************************************
+	
+	/***************************************************************************
+	 * Getters *****************************************************************
+	 **************************************************************************/ 
 	
 	public bool isTag(){
 		return this.istag;
@@ -142,10 +210,6 @@ class HTMLElement{
 	public bool isNonPairTag(){
 		return this.isnonpairtag;
 	}
-
-	public void setIsNonPairTag(bool isnonpairtag){
-		this.isnonpairtag = isnonpairtag;
-	}
 	
 	public bool isComment(){
 		return this.iscomment;
@@ -158,211 +222,221 @@ class HTMLElement{
 	public string getTagName(){
 		return this.tagname;
 	}
+	//* /Getters ***************************************************************
+	
+	/***************************************************************************
+	 * Setters *****************************************************************
+	 **************************************************************************/ 
+	
+	public void isNonPairTag(bool isnonpairtag){
+		this.isnonpairtag = isnonpairtag;
+		this.endtag = null;
+		this.childs = null;
+	}
+	//* /Setters ***************************************************************
 }
 
 
-class HTMLParser{
-	/**
-	 * Parse HTML from text into array filled with tags end text.
-	 * 
-	 * Source code is little bit unintutive, because it is simple parser machine.
-	 * For better understanding, look at; http://kitakitsune.org/images/field_parser.png
-    */ 
-	private static string[] raw_split(string itxt){
-		char echr;
-		char[4] buff;
-		string content;
-		string[] array;
-		ubyte next_state = 0;
-		bool inside_tag = false;
-		
-		foreach(char c; itxt){
-			switch(next_state){
-				case 0: // content
-					if (c == '<'){
-						if (content.length > 0)
-							array ~= content;
-						content = "" ~ c;
-						next_state = 1;
-						inside_tag = false;
-					}else{
-						content ~= c;
-					}
-
-					break;
-				case 1: // html tag
-					if (c == '>'){
-						array ~= content ~ c;
-						content = "";
-						next_state = 0;
-					}else if (c == '\'' || c == '"'){
-						echr = c;
-						content ~= c;
-						next_state = 2;
-					}else if (c == '-' && buff[0] == '-' && buff[1] == '!' && buff[2] == '<'){
-						if (content[0 .. ($ - 3)].length > 0)
-							array ~= content[0 .. ($ - 3)];
-						content = content[($ - 3) .. $] ~ c;
-						next_state = 3;
-					}else{
-						if (c == '<') // jump back into tag instead of content
-							inside_tag = true;
-						content ~= c;
-					}
-					
-					break;
-				case 2: // "" / ''
-					if (c == echr && (buff[0] != '\\' || (buff[0] == '\\' && buff[1] == '\\'))){
-						next_state = 1;
-					}
+/**
+ * Parse HTML from text into array filled with tags end text.
+ * 
+ * Source code is little bit unintutive, because it is simple parser machine.
+ * For better understanding, look at; http://kitakitsune.org/images/field_parser.png
+*/ 
+private string[] raw_split(string itxt){
+	char echr;
+	char[4] buff;
+	string content;
+	string[] array;
+	ubyte next_state = 0;
+	bool inside_tag = false;
+	
+	foreach(char c; itxt){
+		switch(next_state){
+			case 0: // content
+				if (c == '<'){
+					if (content.length > 0)
+						array ~= content;
+					content = "" ~ c;
+					next_state = 1;
+					inside_tag = false;
+				}else{
 					content ~= c;
-					
-					break;
-				case 3: // html comments
-					if (c == '>' && buff[0] == '-' && buff[1] == '-'){
-						if (inside_tag)
-							next_state = 1;
-						else
-							next_state = 0;
-						inside_tag = false;
-						
-						array ~= content ~ c;
-						content = "";
-					}else{
-						content ~= c;
-					}
-					
-					break;
-			}
-			
-			// rotate buffer
-			for(int i = buff.length - 1; i > 0; i--){
-				buff[i] = buff[i - 1];
-			}
-			buff[0] = c;
-			
-		}
-		
-		if (content.length > 0)
-			array ~= content;
-		
-		return array;
-	}
-
-	/**
-	 * Repair tags with comments (<HT<!-- asad -->ML> is parsed to ["<HT", "<!-- asad -->", "ML>"]
-	 * and I need ["<HTML>", "<!-- asad -->"])
-	*/ 
-	private static HTMLElement[] repairTags(HTMLElement[] raw_input){
-		HTMLElement[] ostack;
-		
-		foreach(uint index, HTMLElement el; raw_input){
-			if (el.isComment()){
-				if (index > 0 && index < raw_input.length){
-					if (raw_input[index - 1].toString().startsWith("<") && raw_input[index + 1].toString().endsWith(">")){
-						ostack[$ - 1] = new HTMLElement(ostack[$ - 1].toString() ~ raw_input[index + 1].toString());
-						ostack ~= el;
-						index += 1;
-						continue;
-					}
 				}
-			}
 
-			ostack ~= el;
+				break;
+			case 1: // html tag
+				if (c == '>'){
+					array ~= content ~ c;
+					content = "";
+					next_state = 0;
+				}else if (c == '\'' || c == '"'){
+					echr = c;
+					content ~= c;
+					next_state = 2;
+				}else if (c == '-' && buff[0] == '-' && buff[1] == '!' && buff[2] == '<'){
+					if (content[0 .. ($ - 3)].length > 0)
+						array ~= content[0 .. ($ - 3)];
+					content = content[($ - 3) .. $] ~ c;
+					next_state = 3;
+				}else{
+					if (c == '<') // jump back into tag instead of content
+						inside_tag = true;
+					content ~= c;
+				}
+				
+				break;
+			case 2: // "" / ''
+				if (c == echr && (buff[0] != '\\' || (buff[0] == '\\' && buff[1] == '\\'))){
+					next_state = 1;
+				}
+				content ~= c;
+				
+				break;
+			case 3: // html comments
+				if (c == '>' && buff[0] == '-' && buff[1] == '-'){
+					if (inside_tag)
+						next_state = 1;
+					else
+						next_state = 0;
+					inside_tag = false;
+					
+					array ~= content ~ c;
+					content = "";
+				}else{
+					content ~= c;
+				}
+				
+				break;
 		}
-
-		return ostack;
-	}
-
-	/**
-	 * Go trought istack and search endtag. Element at first index is considered as opening tag.
-	 *
-	 * Returns: index of end tag or 0 if not found.
-	*/ 
-	private static uint indexOfEndTag(HTMLElement[] istack){
-		if (istack.length <= 0)
-			return 0;
-
-		if (!istack[0].isOpeningTag())
-			return 0;
-
-		HTMLElement opener = istack[0];
-		uint cnt = 0;
 		
-		foreach(uint index, HTMLElement el; istack[1 .. $]){
-			if (el.isOpeningTag() && (el.getTagName().tolower() == opener.getTagName().tolower()))
-				cnt++;
-			else if (el.isEndTagTo(opener))
-				if (cnt == 0)
-					return index + 1;
-				else
-					cnt--;
+		// rotate buffer
+		for(int i = buff.length - 1; i > 0; i--){
+			buff[i] = buff[i - 1];
 		}
-
-		return 0;
-	}
-
-	/**
-	 * Recursively go trought element array and create DOM.
-    */ 
-	private static HTMLElement[] parseDOM(HTMLElement[] istack){
-		uint end_tag_index;
-		HTMLElement[] ostack;
+		buff[0] = c;
 		
-		foreach(uint index, HTMLElement el; istack){
-			end_tag_index = indexOfEndTag(istack[index .. $]); // Check if this is pair tag
-
-			if (!el.isNonPairTag && end_tag_index == 0 && !el.isEndTag())
-				el.setIsNonPairTag(true);
-
-			if (end_tag_index != 0){
-				el.childs = parseDOM(istack[index + 1 .. end_tag_index + index]);
-				el.endtag = istack[end_tag_index + index]; // Rreference to endtag
-				el.endtag.openertag = el; // Reference to openertag
-				ostack ~= el;
-				ostack ~= el.endtag;
-				index = end_tag_index + index;
-			}else
-				ostack ~= el;
-		}
-
-		return ostack;
-	}
-
-	public static void pretiffy(HTMLElement[] istack, string separator = "  ", uint depth = 0){
-		foreach(el; istack){
-			for (uint i = 0; i < depth; i++)
-				write(separator);
-
-			writeln(el);
-			
-			if (el.childs.length > 0)
-				pretiffy(el.childs, separator, depth + 1);
-		}
 	}
 	
-	public static HTMLElement[] parseString(ref string txt){
-		HTMLElement[] istack;
-		
-		// Convert array of strings to HTMLElements
-		foreach(string el; raw_split(txt)){
-			istack ~= new HTMLElement(el);
+	if (content.length > 0)
+		array ~= content;
+	
+	return array;
+}
+
+/**
+ * Repair tags with comments (<HT<!-- asad -->ML> is parsed to ["<HT", "<!-- asad -->", "ML>"]
+ * and I need ["<HTML>", "<!-- asad -->"])
+*/ 
+private HTMLElement[] repairTags(HTMLElement[] raw_input){
+	HTMLElement[] ostack;
+	
+	foreach(uint index, HTMLElement el; raw_input){
+		if (el.isComment()){
+			if (index > 0 && index < raw_input.length){
+				if (raw_input[index - 1].toString().startsWith("<") && raw_input[index + 1].toString().endsWith(">")){
+					ostack[$ - 1] = new HTMLElement(ostack[$ - 1].toString() ~ raw_input[index + 1].toString());
+					ostack ~= el;
+					index += 1;
+					continue;
+				}
+			}
 		}
 
-		return parseDOM(repairTags(istack));
+		ostack ~= el;
 	}
+
+	return ostack;
+}
+
+/**
+ * Go through istack and search endtag. Element at first index is considered as opening tag.
+ *
+ * Returns: index of end tag or 0 if not found.
+*/ 
+private uint indexOfEndTag(HTMLElement[] istack){
+	if (istack.length <= 0)
+		return 0;
+
+	if (!istack[0].isOpeningTag())
+		return 0;
+
+	HTMLElement opener = istack[0];
+	uint cnt = 0;
+	
+	foreach(uint index, HTMLElement el; istack[1 .. $]){
+		if (el.isOpeningTag() && (el.getTagName().tolower() == opener.getTagName().tolower()))
+			cnt++;
+		else if (el.isEndTagTo(opener))
+			if (cnt == 0)
+				return index + 1;
+			else
+				cnt--;
+	}
+
+	return 0;
+}
+
+/**
+ * Recursively go through element array and create DOM.
+*/ 
+private HTMLElement[] parseDOM(HTMLElement[] istack){
+	uint end_tag_index;
+	HTMLElement[] ostack;
+	
+	foreach(uint index, HTMLElement el; istack){
+		end_tag_index = indexOfEndTag(istack[index .. $]); // Check if this is pair tag
+
+		if (!el.isNonPairTag && end_tag_index == 0 && !el.isEndTag())
+			el.isNonPairTag(true);
+
+		if (end_tag_index != 0){
+			el.childs = parseDOM(istack[index + 1 .. end_tag_index + index]);
+			el.endtag = istack[end_tag_index + index]; // Rreference to endtag
+			el.endtag.openertag = el; // Reference to openertag
+			ostack ~= el;
+			ostack ~= el.endtag;
+			index = end_tag_index + index;
+		}else
+			ostack ~= el;
+	}
+
+	return ostack;
+}
+
+public void pretiffy(HTMLElement[] istack, string separator = "  ", uint depth = 0){
+	foreach(el; istack){
+		for (uint i = 0; i < depth; i++)
+			write(separator);
+
+		writeln(el);
+		
+		if (el.childs.length > 0)
+			pretiffy(el.childs, separator, depth + 1);
+	}
+}
+	
+public static HTMLElement[] parseString(ref string txt){
+	HTMLElement[] istack;
+	
+	// Convert array of strings to HTMLElements
+	foreach(string el; raw_split(txt)){
+		istack ~= new HTMLElement(el);
+	}
+
+	return parseDOM(repairTags(istack));
 }
 
 
 
 void main(){
-	HTMLElement[] dom = HTMLParser.parseString(
+	HTMLElement[] dom = parseString(
 		"<doctype sracky...>" ~
 		"<HTML>" ~
 		"<head <!-- Doplnit meta tagy!--> >" ~
 		"<title>Testovaci polygon..</title>" ~
 		"</head>" ~
-		"<body bgcolor='black'>" ~
+		"<body bgcolor='black'asd=bsd>" ~
 		"<h1>Polygon..</h1>" ~
 		"Nejaky pekny odstavecek.." ~
 		"<!-- zakomentovany text.. >>><<<< \" -->"
@@ -372,5 +446,5 @@ void main(){
 		"</html>"
 	);
 
-	HTMLParser.pretiffy(dom);
+	pretiffy(dom);
 }
