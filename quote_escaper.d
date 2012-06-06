f04512aa8dbaf9be4b545e6d72b9cf6835c13e39
Bystroushaak <bystrousak@kitakitsune.org>
2012-06-06 18:06:22 +0200

    Updated quote_escaper -> unescape.

--- quote_escaper.d
+++ quote_escaper.d
@@ -1,26 +1,26 @@
-/* quote_escaper.d v1.1.1 (14.09.2011) by Bystroushaak (bystrousak@kitakitsune.org)
+/* quote_escaper.d v1.2.0 (06.06.2011) by Bystroushaak (bystrousak@kitakitsune.org)
  * 
  * Module for (un)escaping quotes.
  * 
  * This work is licensed under a CC BY (http://creativecommons.org/licenses/by/3.0/)
 */
+	import std.stdio;
 public string unescape(string input, char quote = '"'){
 	if (input.length < 2)
 		return input;
 	
 	string output;
-	char old = input[0];
-	char older;
-	foreach(act; input[1 .. $] ~ ' '){
-		if (act == quote && old == '\\' && older != '\\'){
-			older = old;
-			old = act;
-			continue;
-		}else
-			output ~= old;
-
-		older = old;
-		old = act;
+	bool unesc = false;
+	foreach(act; input){
+		if (act == quote && unesc)
+			output = output[0 .. $-1];
+		
+		output ~= act;
+		
+		if (act == '\\')
+			unesc = !unesc;
+		else
+			unesc = false;
 	}
 
 	return output;
@@ -40,9 +40,12 @@ public string escape(string input, char quote = '"'){
 }
 
 unittest{
-    assert(unescape(`\' \\ \" \n`) == `\' \\ " \n`);
-    assert(unescape(`\' \\ \" \n`, '\'') == `' \\ \" \n`);
-    
-    assert(escape(`printf("hello world");`) == `printf(\"hello world\");`);
-    assert(escape(`'`, '\'') == `\'`);
+		assert(unescape(`\' \\ \" \n`) == `\' \\ " \n`);
+		assert(unescape(`\' \\ \" \n`, '\'') == `' \\ \" \n`);
+		assert(unescape(`\' \\" \n`) == `\' \\" \n`);
+		assert(unescape(`\' \\" \n`) == `\' \\" \n`);
+		assert(unescape(`printf(\"hello \t world\");`) == `printf("hello \t world");`);
+		
+		assert(escape(`printf("hello world");`) == `printf(\"hello world\");`);
+		assert(escape(`'`, '\'') == `\'`);
 }
